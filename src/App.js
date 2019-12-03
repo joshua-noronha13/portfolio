6754e2127622faf5a146d50fe09eb6b72e2e145a
Your Name <you@example.com>
2019-12-03 23:34:04 +0530

    React project created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Particles from 'react-particles-js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="particle-js-container">
+        <Particles
+          style={{
+            display: 'flex',
+            width: '100%',
+            height: '20%',
+            background: `linear-gradient(to right, #000046, #1CB5E0)`
+          }} />
+      </div>
+      <div className="container">
+        <h1 id="heading">Joshua Noronha</h1>
+
+        <div className="card">
+          <div className="card-image"><img src="./img/smart_street_lamps.jpeg" /></div>
+          <div className="card-text">Smart Street Lamps</div>
+        </div>
+        <div className="card-hide">
+          <i className="fas fa-play-circle"></i> Click here
+        </div>
+        <div className="card">
+          <div className="card-image"><img src="./img/smart_street_lamps.jpeg" /></div>
+          <div className="card-text">Smart Street Lamps</div>
+        </div>
+        <div className="card">
+          <div className="card-image"><img src="./img/smart_street_lamps.jpeg" /></div>
+          <div className="card-text">Smart Street Lamps</div>
+        </div>
+      </div>
     </div>
   );
 }
