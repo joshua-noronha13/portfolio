fb071fbacb6dc5691a1fed0be68d7702fdf4672d
Your Name <you@example.com>
2019-12-05 23:58:28 +0530

    Added images to the project and changed the UI

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Particles from "react-particles-js";
 import styled from "styled-components";
-
+import smart_street_lamp from "./img/smart_street_lamp.png"
+import satellite_image from "./img/map.png"
+import camping from "./img/camping.png"
 function App() {
   const Overlay = styled.div`
     position: absolute;
     bottom: 0;
     left: 0;
     right: 0;
-    background-color: #008cba;
+    background-color: #fc5185;
     overflow: hidden;
     width: 100%;
     height: 0;
@@ -18,8 +19,9 @@ function App() {
     transition: 0.5s ease;
   `;
   const OverlayText = styled.div`
-    margin: auto;
+    text-align:center;
     color: white;
+    margin-top:35%;
   `;
   const Card = styled.div`
     display: inline-block;
@@ -42,35 +44,36 @@ function App() {
       <div className="particle-js-container">
         <Particles
           width="100vw"
-          height="50vh"
+          height="140vh"
           style={{
-            background: `linear-gradient(to right, #000046, #1CB5E0)`
+            background: `#364f6b`
           }}
         />
       </div>
+      <div className="random">Hi, I'm Joshua</div>
       <div className="container">
         <h1 id="heading">ACD</h1>
 
         <Card>
           <div className="card-image">
-            <img src="./img/smart_street_lamps.jpeg" />
+            <img src={smart_street_lamp} />
           </div>
           <div className="card-text">Smart Street Lamps</div>
-          <Overlay>View More</Overlay>
+          <Overlay><OverlayText>View More</OverlayText></Overlay>
         </Card>
         <Card>
           <div className="card-image">
-            <img src="./img/smart_street_lamps.jpeg" />
+            <img src={satellite_image} />
           </div>
-          <div className="card-text">Smart Street Lamps</div>
-          <Overlay>Hi</Overlay>
+          <div className="card-text">Semantic Segmentation of Satellite Tiles</div>
+          <Overlay><OverlayText>View More</OverlayText></Overlay>
         </Card>
         <Card>
           <div className="card-image">
-            <img src="./img/smart_street_lamps.jpeg" />
+            <img src={camping} />
           </div>
           <div className="card-text">Smart Street Lamps</div>
-          <Overlay>Hi</Overlay>
+          <Overlay><OverlayText>View More</OverlayText></Overlay>
         </Card>
       </div>
     </div>
