dc2dc64ab1ed87e4cc29518c283280f371b01f14
Dhruvit Galoriya <d.galoriya@tcs.com>
2019-12-05 19:14:17 +0530

    add styled component css

--- src/App.js
+++ src/App.js
@@ -5,8 +5,25 @@ import Particles from "react-particles-js";
 import styled from "styled-components";
 
 function App() {
+  const Overlay = styled.div`
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: #008cba;
+    overflow: hidden;
+    width: 100%;
+    height: 0;
+    border-radius: 10px;
+    transition: 0.5s ease;
+  `;
+  const OverlayText = styled.div`
+    margin: auto;
+    color: white;
+  `;
   const Card = styled.div`
     display: inline-block;
+    position: relative;
     width: 31%;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     margin: 1%;
@@ -15,6 +32,9 @@ function App() {
     background-color: white;
     transition: all 0.5s ease-in-out;
     color: palevioletred;
+    &:hover ${Overlay} {
+      height: 100%;
+    }
   `;
 
   return (
@@ -29,28 +49,28 @@ function App() {
         />
       </div>
       <div className="container">
-        <h1 id="heading">Joshua Noronha</h1>
+        <h1 id="heading">ACD</h1>
 
         <Card>
           <div className="card-image">
             <img src="./img/smart_street_lamps.jpeg" />
           </div>
           <div className="card-text">Smart Street Lamps</div>
+          <Overlay>View More</Overlay>
         </Card>
-        <div className="card-hide">
-          <i className="fas fa-play-circle"></i> Click here
-        </div>
         <Card>
           <div className="card-image">
             <img src="./img/smart_street_lamps.jpeg" />
           </div>
           <div className="card-text">Smart Street Lamps</div>
+          <Overlay>Hi</Overlay>
         </Card>
-        <Card className="card">
+        <Card>
           <div className="card-image">
             <img src="./img/smart_street_lamps.jpeg" />
           </div>
           <div className="card-text">Smart Street Lamps</div>
+          <Overlay>Hi</Overlay>
         </Card>
       </div>
     </div>
