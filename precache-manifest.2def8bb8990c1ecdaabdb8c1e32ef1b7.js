0872f8b1d9ac79e1393964bbfb20c6a17716c75f
Your Name <you@example.com>
2019-12-06 00:02:13 +0530

    Updates

---
+++ precache-manifest.2def8bb8990c1ecdaabdb8c1e32ef1b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bba188b4c2318ddad116eed2fb2bdae",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3d3f956a6e472705eb66",
+    "url": "/portfolio/static/css/main.7441f35f.chunk.css"
+  },
+  {
+    "revision": "cdf9addda603a1c259f0",
+    "url": "/portfolio/static/js/2.5e09d3ea.chunk.js"
+  },
+  {
+    "revision": "3d3f956a6e472705eb66",
+    "url": "/portfolio/static/js/main.572880c9.chunk.js"
+  },
+  {
+    "revision": "a694c17334d77ca95504",
+    "url": "/portfolio/static/js/runtime-main.eaf37d0e.js"
+  },
+  {
+    "revision": "33bed616974c6f291fdf73e8b3155f55",
+    "url": "/portfolio/static/media/camping.33bed616.png"
+  },
+  {
+    "revision": "c0eb1a543d8767bfeda3d61faf00c2bf",
+    "url": "/portfolio/static/media/map.c0eb1a54.png"
+  },
+  {
+    "revision": "dd274aa0cae7f40dddf3202a3723d095",
+    "url": "/portfolio/static/media/smart_street_lamp.dd274aa0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.4891da169ac8c332fb73403f64797c2d.js"
+  "/portfolio/precache-manifest.2def8bb8990c1ecdaabdb8c1e32ef1b7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
