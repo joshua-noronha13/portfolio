e8398492c6ee04f581c82ebff86261098c8958d3
Joshua Noronha <noronhajoshua1@gmail.com>
2020-06-27 20:30:22 +0530

    Updates

---
+++ precache-manifest.7cc765a4552fdc2ce839986ccfa97e1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e508d1709148a1a1d79eab541ca86a4d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f7907796b79cb2e3a55f",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c15b8ea0776015a43a3f",
+    "url": "/portfolio/static/css/main.9b3a3bbf.chunk.css"
+  },
+  {
+    "revision": "f7907796b79cb2e3a55f",
+    "url": "/portfolio/static/js/2.85f0fc13.chunk.js"
+  },
+  {
+    "revision": "b5de1b7a86bcbacb2ceb74f2733391d3",
+    "url": "/portfolio/static/js/2.85f0fc13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c15b8ea0776015a43a3f",
+    "url": "/portfolio/static/js/main.f546eaae.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0400d3ad4c6b538f4bc1fd54ba99a2ef.js"
+  "/portfolio/precache-manifest.7cc765a4552fdc2ce839986ccfa97e1f.js"
 );
 
 self.addEventListener('message', (event) => {
