5f5befc1009201e498ba7c2e32058014fd8b8546
Joshua Noronha <noronhajoshua1@gmail.com>
2020-08-30 19:41:57 +0530

    Minor bug fixes, seperated data, footer now has contact links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import Icon from "../BasicComponents/Icon"
 export default function About() {
     return (
         <div style={{ textAlign: "center", marginTop: "5em", marginBottom: "5px" }}>
-            <div style={{ color: "var(--secondary)" }}>Made with <span class="material-icons" style={{ fontSize: "0.9rem" }}>favorite</span> by Joshua Noronha</div>
+            <div style={{ color: "var(--secondary)" }}>Made with <span className="material-icons" style={{ fontSize: "0.9rem" }}>favorite</span> by Joshua Noronha</div>
             <div className={"iconbar"}>
                 <Icon
                     url="https://www.linkedin.com/in/joshua-noronha/"
